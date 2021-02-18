a0390cd28a35db54d1259d2cfdf4ba9ccecf3707
kedwards413 <kaela.rose.edwards@gmail.com>
2021-02-18 01:58:26 +0000

    learning python

--- src/app.py
+++ src/app.py
@@ -1,7 +1,7 @@
 """
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
-import os
+import os, json
 from flask import Flask, request, jsonify, url_for
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
@@ -31,13 +31,39 @@ def handle_hello():
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
     response_body = {
-        "hello": "world",
+
         "family": members
     }
 
+    return jsonify(response_body), 200
+
+@app.route('/member', methods=['POST'])
+def add_member():
+    member = request.json
+    jackson_family.add_member(member)
+    members = jackson_family.get_all_members()
+    response_body = {
+        "family": members
+    }
+
+    # this is how you can use the Family datastructure by calling its methods
+    
 
     return jsonify(response_body), 200
 
+@app.route('/member/<int:member_id>', methods=['DELETE'])
+def delete_member():
+    deleted_member = request.json
+    jackson_family.delete_member(deleted_member)
+    members = jackson_family.get_all_members()
+    response_body = {
+        "family": members
+    }
+
+    # this is how you can use the Family datastructure by calling its methods
+    
+
+    return jsonify(response_body), 200
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
--- src/datastructures.py
+++ src/datastructures.py
@@ -13,7 +13,11 @@ class FamilyStructure:
         self.last_name = last_name
 
         # example list of members
-        self._members = []
+        self._members = [{ "id": 3,
+    "first_name": "Kaela",
+    "last_name": "Edwards",
+    "age": 29,
+    "lucky_numbers": 3}]
 
     # read-only: Use this method to generate random members ID's when adding members into the list
     def _generateId(self):
@@ -21,11 +25,15 @@ class FamilyStructure:
 
     def add_member(self, member):
         # fill this method and update the return
-        pass
+        return self._members.append(member)
+        
 
     def delete_member(self, id):
         # fill this method and update the return
-        pass
+        for index in range(len(self._members)):
+            if id == self._members[index].id:
+                removed = self._members.pop(index)
+                return removed
 
     def get_member(self, id):
         # fill this method and update the return
